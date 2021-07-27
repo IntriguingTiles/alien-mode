6a5669bd0b11f9ce8d2cdd7b7b8c3c1fe872d998
IntriguingTiles <jnickw@outlook.com>
2021-07-26 22:00:39 -0600

    Disable player_weaponstrip

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -4587,19 +4587,7 @@ LINK_ENTITY_TO_CLASS( player_weaponstrip, CStripWeapons );
 
 void CStripWeapons :: Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
 {
-	CBasePlayer *pPlayer = NULL;
-
-	if ( pActivator && pActivator->IsPlayer() )
-	{
-		pPlayer = (CBasePlayer *)pActivator;
-	}
-	else if ( !g_pGameRules->IsDeathmatch() )
-	{
-		pPlayer = (CBasePlayer *)CBaseEntity::Instance( g_engfuncs.pfnPEntityOfEntIndex( 1 ) );
-	}
-
-	if ( pPlayer )
-		pPlayer->RemoveAllItems( FALSE );
+	return;
 }
 
 
